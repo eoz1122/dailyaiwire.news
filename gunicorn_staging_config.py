de1db0701c1ec9e61a3c772e03537bb42b53b265
Emre <emreozen1122@hotmail.com>
2025-12-26 00:54:45 +0100

    chore: add gunicorn staging config

---
+++ gunicorn_staging_config.py
@@ -0,0 +1,5 @@
+bind = "127.0.0.1:8001"
+workers = 2
+loglevel = "info"
+errorlog = "/home/dailyai/staging.dailyaiwire.news/logs/gunicorn-error.log"
+accesslog = "/home/dailyai/staging.dailyaiwire.news/logs/gunicorn-access.log"
