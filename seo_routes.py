4ddba2f7a94eb232c8d39764e47f8363b2daf775
Emre <emreozen1122@hotmail.com>
2025-12-26 00:15:32 +0100

    fix(sitemap): fix database connection leak in sitemap generation

--- seo_routes.py
+++ seo_routes.py
@@ -44,7 +44,9 @@ def sitemap():
     xml.append('</url>')
 
     # Categories
+    conn = get_db_connection()
     categories = conn.execute('SELECT category FROM articles WHERE category IS NOT NULL GROUP BY category').fetchall()
+    conn.close()
     for cat in categories:
         xml.append('<url>')
         xml.append(f'<loc>https://dailyaiwire.news/?category={cat["category"]}</loc>')
