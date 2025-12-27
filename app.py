6f1feb1bcabab8d38bb4a6ba117341483545a06e
Emre <emreozen1122@hotmail.com>
2025-12-27 21:00:06 +0100

    Add Contact page with info@dailyaiwire.news

--- app.py
+++ app.py
@@ -160,6 +160,9 @@ def article(slug):
 @app.route('/about')
 def about(): return render_template('about.html')
 
+@app.route('/contact')
+def contact(): return render_template('contact.html')
+
 @app.route('/privacy')
 def privacy(): return render_template('privacy.html')
 
